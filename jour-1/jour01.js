f9c3c73db4a92778ec2478c5d5a90bf9f6277a09
Antoine <bonjour@antoine.cool>
2021-03-08 16:03:24 +0100

    hi

---
+++ jour-1/jour01.js
@@ -0,0 +1,31 @@
+// var test= "My name is Rocio"
+// console.log(test);
+
+// var prenom= "Rocio"
+// console.log('Nice to meet you '+ prenom);
+
+// var testLength="I'm very long !";
+// console.log(test.length);
+
+// var food= "croissant is meh"
+// food = food.replace("meh", "good");
+// console.log(food);
+
+// var basic= "This is Cool"
+// var basicUp= basic.toUpperCase();
+// console.log(basicUp);
+
+// var basicDown= basic.toLowerCase();
+// console.log(basicDown);
+
+// var word= "banana"
+// var letters = word.split("");
+// console.log(letters);
+
+// var age= 29;
+// var prenom= "Rocio";
+// console.log(`I'm ${prenom} ${age} years old`);
+
+var changed= "Bonjour"
+changed = changed.replace("o","a","a","a","a");
+console.log(changed);
