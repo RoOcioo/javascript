99e00d0e71a2d67f8e4245177bb2e90ac9e41cfa
Antoine <bonjour@antoine.cool>
2021-03-12 13:53:07 +0100

    hi

--- jour-4/ex6.js
+++ jour-4/ex6.js
@@ -1,12 +1,8 @@
 function format(time) {
-    var hours = Math.floor(time*60);
-    var minutes = time % 3600;
-    var seconds = time % 60;
+    var hours = Math.floor(time / 3600 );
+    var minutes = Math.floor((time % 3600) / 60);
+    var seconds = (time % 3600)  % 60;
 
-    if (hours < 10) {hours = "0"+hours;}
-    if (minutes < 10) {minutes = "0"+minutes;}
-    if (seconds < 10) {seconds = "0"+seconds;}    
-
-    return hours+':' +minutes+':'+ ':'+seconds;
+console.log( hours+':' +minutes+':'+seconds);
 }
 format(3700);
