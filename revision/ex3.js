b2f0f3d75eabf83649bb0ba2fb1e4cc2f57b380b
Antoine <bonjour@antoine.cool>
2021-03-15 18:21:41 +0100

    js

--- revision/ex3.js
+++ revision/ex3.js
@@ -1,18 +1,14 @@
-function checkPal(str) {
-var tab1 = str.split("");
-console.log(tab1);
-var tab2 = tab1.reverse();
-console.log(tab2);
+function checkPal(word) {
+var reverse = word.split("").reverse().join("");
+console.log(reverse);
 
-for (var i = 0; i < tab1.length; i++) {
-    if (tab1[i] === tab2[tab1.length - 1 - i]) {
-        return "Palindrome !";
-    } else {
-        return "Nope !";
-    }
-}
+if (word === reverse) {
+        console.log( "Palindrome !");
+      } else {
+        console.log( "Nope !");
+      }
+  }
 
-}
 
-console.log(checkPal("koptpok"));
+checkPal("kasyak");
 
